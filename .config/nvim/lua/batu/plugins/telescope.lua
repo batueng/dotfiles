64a074d8a932bd8f28fe15a2da230870bbbbca6b
Batu Engin <batueng@umich.edu>
2025-06-02 20:28:24 -0700

    be gone nvim tree, welcome space fn

--- .config/nvim/lua/batu/plugins/telescope.lua
+++ .config/nvim/lua/batu/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -25,11 +26,13 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("file_browser")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
     keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fn", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", { desc = "Filebrowser" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
